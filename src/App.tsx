8f5ca1636d4e8fe6ef37f794bd8aa8602ad02bf2
EL-OUARDY <ouadie.elouardy@gmail.com>
2026-03-16 00:28:03 +0000

    feat: change scrollbar appearance

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import Header from './Components/Header';
+
 function App() {
-  return <h1 className="text-red-500">Hello World</h1>;
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="h-dvh"></div>
+        <div className="h-dvh bg-purple-300"></div>
+        <div className="h-dvh bg-orange-300"></div>
+        <div className="h-dvh bg-green-300"></div>
+        <div className="h-dvh bg-red-300"></div>
+      </main>
+
+      {/* <Footer /> */}
+    </>
+  );
 }
 
 export default App;
