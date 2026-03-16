48716869cd27d6f71bdaeb1d3f1e307464a02bf0
EL-OUARDY <ouadie.elouardy@gmail.com>
2026-03-16 00:14:06 +0000

    feat: add Container component

---
+++ src/Components/Container.tsx
@@ -0,0 +1,24 @@
+import { type ElementType, type ReactNode, forwardRef } from 'react';
+import { cn } from '../lib/utils';
+
+interface Props {
+  as?: ElementType;
+  className?: string;
+  children: ReactNode;
+}
+
+export const Container = forwardRef<HTMLElement, Props>(
+  ({ as: Comp = 'section', className, children, ...rest }, ref) => {
+    return (
+      <Comp
+        ref={ref}
+        className={cn('overflow-hidden px-6', className)}
+        {...rest}
+      >
+        <div className="mx-auto w-full max-w-270">{children}</div>
+      </Comp>
+    );
+  },
+);
+
+Container.displayName = 'Container';
